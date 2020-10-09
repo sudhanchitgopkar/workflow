a245aaaf08e734f61b3afc8aa928a45a276d59d8
sudhan.chitgopkar <sudhanc03@yahoo.com>
2020-10-08 20:01:41 -0400

    10.07.20.02

--- school/csci/assn04/bitsum.cpp
+++ school/csci/assn04/bitsum.cpp
@@ -2,13 +2,13 @@
 #include "bitsum.h"
 
 int numberOfOnes(unsigned long int value) {
-
   int rem = 0;
   int numOfOnes = 0;
-  
+
   while (value >= 1) {
     rem = value % 2;
-    value = (int) value / 2;
+    value =  value / 2;
+   // std::cout << "value: "<< value << std::endl;
     if (rem == 1) {
       numOfOnes++;
     } //if
