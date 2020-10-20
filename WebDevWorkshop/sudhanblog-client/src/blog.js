09a3a6f5d0a9bece56b8c2c2c31150c16256d2d3
sudhan.chitgopkar <sudhanc03@yahoo.com>
2020-10-19 20:03:44 -0400

    10.19.20

---
+++ WebDevWorkshop/sudhanblog-client/src/blog.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Component} from 'react'
+
+class blog extends Component {
+    render() {
+        <h1> Blog Page </h1>
+    } //render
+} //home
+
+export default blog
